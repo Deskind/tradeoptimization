ad294cd5b2c5ac14453d83d9122adbc62fcc67c0
Deskind <desk1nd88@gmail.com>
2017-03-29 22:22:45 +0300

    draw a totalchart bug with sessions management

--- src/main/java/com/deskind/tradeoptimization/controllers/MainController.java
+++ src/main/java/com/deskind/tradeoptimization/controllers/MainController.java
@@ -13,15 +13,18 @@ import java.util.List;
 import java.util.ResourceBundle;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.stage.FileChooser;
 import javafx.stage.Popup;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import sun.misc.Signal;
 
 
 public class MainController implements Initializable {
@@ -31,6 +34,9 @@ public class MainController implements Initializable {
     
     @FXML
     private Button makeChartBtn;
+    
+    @FXML
+    private LineChart<String, Number> totalChart;
     
     @FXML
     void handleChooseFilesBtn(ActionEvent event) {
@@ -47,9 +53,9 @@ public class MainController implements Initializable {
         //The most latest date in Sgn table
         LocalDate latestDate = null;
         //The variable contains profit in percents
-        Float balance;
+        Float totalBalance = 0f;
         //Variable contains series of data for every trade pair
-        List<XYChart.Series<String, Number>> series = new ArrayList<>();
+        XYChart.Series series = new XYChart.Series();
         /**Hibernate's update session factory*/
         HibernateUtils.getSessionFactory("hibernate_update.cfg.xml");
         
@@ -63,25 +69,36 @@ public class MainController implements Initializable {
             latestDate = latestSignal.getDate().toLocalDate();
         //end
         
-        /**Getting list of trade pairs*/
-        List tradePairs = HibernateUtils.queryForMe("FROM TradePair");
-        //End
-        
-        /**Iterate over trade pairs*/
-        for(Object t : tradePairs){
-            LocalDate date = iteratedDate;
-            TradePair tradePair = (TradePair)t;
-            String tradePairName = tradePair.getName();
-            /**Iterate over all dates*/
-            while(!iteratedDate.equals(latestDate)){
-                List dateSignals = HibernateUtils.queryForMe("from Sgn where pair like '%"+tradePairName+"%' and date like '%"+iteratedDate.toString()+"%'");
-                
+        /**Iterate all days*/
+        while(!iteratedDate.equals(latestDate)){
+            Float balance = 0f;
+            Sgn signal = null;
+            //Get signals for iterated date
+            List daySignals = HibernateUtils.queryForMe("from Sgn where date like '%"+iteratedDate.toString()+"%'");
+            
+            /**Is list empty or not*/
+            if(daySignals.size()==0){
+                iteratedDate = iteratedDate.plusDays(1);
+                continue;
+            }else{
+                for(Object o : daySignals){
+                    signal = (Sgn)o;
+                    if(signal.getResult()==1){
+                        balance+=0.75f;
+                    }else{
+                        balance-=1;
+                    }
+                }
             }
+            totalBalance+=balance;
+            series.getData().add(new XYChart.Data<>(iteratedDate.toString(), totalBalance));
+            iteratedDate = iteratedDate.plusDays(1);
+                System.out.println(series.getData().toString());
             //End
         }
         //End
         
-        
+        totalChart.getData().add(series);
         
         /**Closing hibernate's session factory*/
         HibernateUtils.sessionFactory.close();
@@ -92,8 +109,8 @@ public class MainController implements Initializable {
         
         SqlUtil.createSgnTable();
         
-        SessionFactory sessionFactory = HibernateUtils.getSessionFactory("hibernate.cfg.xml");
-        Session session = sessionFactory.openSession();
+        HibernateUtils.getSessionFactory("hibernate.cfg.xml");
+        Session session = HibernateUtils.sessionFactory.openSession();
         session.beginTransaction();
         
         for(File f : files){
@@ -107,8 +124,7 @@ public class MainController implements Initializable {
         
         session.getTransaction().commit();
         session.close();
-        sessionFactory.close();
-        sessionFactory.close();
+        HibernateUtils.sessionFactory.close();
         
         for(File f : files){
 //            File f = files.get(0);
---
+++ src/main/resources/queries.sql
@@ -0,0 +1,3 @@
+select * from Sgn where date like '%2017-01-02%';
+
+select * from Sgn ORDER BY date ASC;
